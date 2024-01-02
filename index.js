c6356ecbcda2d2bc7310e9fce19401afcb46e065
Ohm.jirawatt <99016810+ohmdevpro@users.noreply.github.com>
2024-01-02 20:32:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 
-// Enable CORS for the specified origin
 app.use(cors({
-  origin: ["https://p1n3r3n.vercel.app", "http://localhost:5173"], // change your domain...
-  methods: ["POST"]
+  origin: "https://p1n3r3n.vercel.app"
 }));
 
-// Use bodyParser middleware to parse JSON
 app.use(bodyParser.json());
 
-// Define your API endpoint
-app.post('/api/getLottery', (req, res) => {
-  // Make a request to the Glo API
-  axios.post('https://www.glo.or.th/api/lottery/getLatestLottery')
-    .then((responseFromGlo) => {
-      // Log the data from Glo API
-      console.log(responseFromGlo.data);
+app.get('/api/lotto/:date', (req, res) => {
+  const date = req.params.date;
+  console.log(date);
 
-      // Send the Glo API response back to the client
-      res.json(responseFromGlo.data);
+  axios.get(`https://lotto.api.rayriffy.com/lotto/${date}`)
+    .then((response) => {
+      res.json(response.data);
+      console.log(response.data);
     })
-    .catch((error) => {
-      console.error(error);
-
-      // Send an error response to the client
-      res.status(500).json({ error: 'Internal Server Error' });
+    .catch((err) => {
+      console.log('Something went wrong...', err);
     });
 });
 
-app.get(`/api/lotto/${date}`, (req, res) => {
-  axios.get(`https://lotto.api.rayriffy.com/lotto/${date}`)
-  .then((res) => {
-    res.json(res.data)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-})
-
-
-// Start the server on port 3000
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server running...');
 });
